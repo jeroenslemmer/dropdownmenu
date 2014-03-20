4fe150ed3c623cc6b7930ec9f82bc6b9c39653e2
Jeroen Slemmer <jeroen.slemmer@gmail.com>
2014-03-20 10:11:53 +0100

    Added submenu

--- src/Program.cs
+++ src/Program.cs
@@ -13,15 +13,26 @@ namespace DropdownMenu
             ConsoleKeyInfo input;
             int currentOption = 0;
             string[] menuText = { "Aperitief", "Voorgerecht", "Hoofdgerecht", "Nagerecht", "Slaapmutsje" };
-
+            string[][] submenuText = {  new string[] { "Aperitief1", "Aperitief2", "Aperitief3" },
+                                        new string[] {"Voorgerecht1", "Voorgerecht2", "Voorgerecht3" },
+                                        new string[] {"Hoofdgerecht1", "Hoofdgerecht2", "Hoofdgerecht3" },
+                                        new string[] { "Nagerecht1", "Nagerecht2", "Nagerecht3" },
+                                        new string[] { "Slaapmutsje1", "Slaapmutsje2", "Slaapmutsje3" }};
+            bool droppedDown = false;
+            int currentCursorLeft = 0;
             do
             {
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.Clear();
+                Console.CursorVisible = false;
+                Console.CursorTop = 0;
+                Console.CursorLeft = 0;
                 for (int i = 0; i < menuText.Length; i++)
                 {
+                    Console.CursorTop = 0;
                     if (i == currentOption)
                     {
+                        currentCursorLeft = Console.CursorLeft;
                         Console.ForegroundColor = ConsoleColor.Black;
                         Console.BackgroundColor = ConsoleColor.White;
                     }
@@ -30,8 +41,17 @@ namespace DropdownMenu
                         Console.ForegroundColor = ConsoleColor.White;
                         Console.BackgroundColor = ConsoleColor.Black;
                     }
-                    Console.Write(menuText[i] + " ");
+                    Console.Write(menuText[i]);
                 }
+                if (droppedDown)
+                {
+                    for (int j = 0; j < submenuText[currentOption].Length; j++)
+                    {
+                        Console.CursorTop = 1 + j;
+                        Console.CursorLeft = currentCursorLeft;
+                        Console.Write(submenuText[currentOption][j]);
+                    }
+                } 
                 input = Console.ReadKey();
                 switch (input.Key)
                 {
@@ -47,7 +67,9 @@ namespace DropdownMenu
                             currentOption--;
                         }
                         break;
-
+                    case ConsoleKey.Enter:
+                        droppedDown = !droppedDown;
+                        break;
                 }
             } while (input.KeyChar != 'q');
         }
