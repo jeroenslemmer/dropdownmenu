2a438a5310cb7b81f16be4600bb029fb5ee45f28
Jeroen Slemmer <jeroen.slemmer@gmail.com>
2014-03-20 09:12:56 +0100

    Project setup

---
+++ src/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropdownMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo input;
+            string[] menuText = { "Aperitief", "Voorgerecht", "Hoofdgerecht", "Nagerecht", "Slaapmutsje" };
+            for (int i = 0; i < menuText.Length; i++)
+            {
+                Console.Write(menuText[i] + " ");
+            }
+            input = Console.ReadKey();
+        }
+    }
+}
