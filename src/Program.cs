d6ebdf9d5b6bf7e966cb1e7894643f059c8fa143
Jeroen Slemmer <jeroen.slemmer@gmail.com>
2014-03-20 09:34:12 +0100

    added highlight for current option and navigation within menu

--- src/Program.cs
+++ src/Program.cs
@@ -11,17 +11,44 @@ namespace DropdownMenu
         static void Main(string[] args)
         {
             ConsoleKeyInfo input;
+            int currentOption = 0;
             string[] menuText = { "Aperitief", "Voorgerecht", "Hoofdgerecht", "Nagerecht", "Slaapmutsje" };
 
             do
             {
+                Console.BackgroundColor = ConsoleColor.Black;
                 Console.Clear();
                 for (int i = 0; i < menuText.Length; i++)
                 {
+                    if (i == currentOption)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
                     Console.Write(menuText[i] + " ");
                 }
-
                 input = Console.ReadKey();
+                switch (input.Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        if (currentOption < menuText.Length - 1)
+                        {
+                            currentOption++;
+                        }
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (currentOption > 0)
+                        {
+                            currentOption--;
+                        }
+                        break;
+
+                }
             } while (input.KeyChar != 'q');
         }
     }
